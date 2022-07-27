62873887c9d6c29046dba5808ac0d1b578e3bec6
christianxnunes <christianxnunes@gmail.com>
2022-07-27 12:19:08 -0300

    Implementing gitignore's challenge and skew

--- 2_Fundamentos/15_Desafio_fundamentos/index.js
+++ 2_Fundamentos/15_Desafio_fundamentos/index.js
@@ -1,1 +1,18 @@
 const chalk = require('chalk') //npm install chalk@4.1.2
+const inquirer = require('inquirer') //npm install inquirer@8.1.0
+
+inquirer.prompt([
+    {
+        name: 'nome',
+        message: 'Qual seu nome?',
+    },
+    {
+        name: 'idade',
+        message: 'Qual a sua idade?',
+    },
+]).then((answers) => {
+    if(!answers.nome || !answers.idade){
+        throw new Error('O nome e a idade são obrigatórios!')
+    }
+    console.log(chalk.bgYellow.black(`Meu nome ${answers.nome} é tenho ${answers.idade} anos`))
+}).catch(err => console.log(chalk.bgRed.black(err)))
